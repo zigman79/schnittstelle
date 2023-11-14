5a4735a78ec7329a807b3fd45374dabce5ffac78
Michael Labuschke <info@futec-gmbh.de>
2023-11-14 20:36:13 +0100

    default route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 Route::post('transfer', [\App\Http\Controllers\DocuWareController::class, 'transfer']);
 Route::post('fileinfo', [\App\Http\Controllers\DocuWareController::class, 'fileinfo']);
-Route::get('transfer', function () {
-    return 'Working';
+Route::fallback(function () {
+    return response()->json();
 });
